07f132bec573c9c05531710bf11b30016239ee8d
Naushikha Jayawickrama <dev@naushikha.com>
2024-05-30 16:03:47 +0530

    Add recaptcha v3

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,20 +72,27 @@ async function submitInquiry() {
     document.getElementById("inq-message").value
   );
 
-  // https://docs.google.com/forms/d/e/1FAIpQLScu-oR4z-ukc8JV3N5oI6BAoKzieSbNIoeDmpCCsnHxDA7k1Q/viewform?usp=pp_url&entry.1345155859=Lunar&entry.641165534=name&entry.1831416741=email&entry.2057905555=subject&entry.1966887127=message
-  const submitURL = `https://docs.google.com/forms/d/e/1FAIpQLScu-oR4z-ukc8JV3N5oI6BAoKzieSbNIoeDmpCCsnHxDA7k1Q/formResponse?&submit=Submit?usp=pp_url&entry.1345155859=${inquiryType}&entry.641165534=${name}&entry.1831416741=${emailAddress}&entry.2057905555=${subject}&entry.1966887127=${message}`;
-  fetch(submitURL)
-    .then((resp) => {
-      console.log(resp);
-    })
-    .catch((err) => {
-      if (err.toString().includes("TypeError: Failed to fetch")) {
-        console.log("success");
-        submitBtn.classList.remove("bg-indigo");
-        submitBtn.classList.add("bg-green");
-        submitBtn.innerHTML =
-          "✔ We've received your message and will get back to you soon!";
-        window.submitDone = true;
-      }
-    });
+  // https://developers.google.com/recaptcha/docs/v3#programmatically_invoke_the_challenge
+  grecaptcha.ready(() => {
+    grecaptcha
+      .execute("6LdziewpAAAAALrXadLzdHEOeTs0WyJl2RTDvI16", { action: "submit" })
+      .then((token) => {
+        // https://docs.google.com/forms/d/e/1FAIpQLScu-oR4z-ukc8JV3N5oI6BAoKzieSbNIoeDmpCCsnHxDA7k1Q/viewform?usp=pp_url&entry.1345155859=Lunar&entry.641165534=name&entry.1831416741=email&entry.2057905555=subject&entry.1966887127=message
+        const submitURL = `https://docs.google.com/forms/d/e/1FAIpQLScu-oR4z-ukc8JV3N5oI6BAoKzieSbNIoeDmpCCsnHxDA7k1Q/formResponse?&submit=Submit?usp=pp_url&entry.1345155859=${inquiryType}&entry.641165534=${name}&entry.1831416741=${emailAddress}&entry.2057905555=${subject}&entry.1966887127=${message}`;
+        fetch(submitURL)
+          .then((resp) => {
+            console.log(resp);
+          })
+          .catch((err) => {
+            if (err.toString().includes("TypeError: Failed to fetch")) {
+              console.log("success");
+              submitBtn.classList.remove("bg-indigo");
+              submitBtn.classList.add("bg-green");
+              submitBtn.innerHTML =
+                "✔ We've received your message and will get back to you soon!";
+              window.submitDone = true;
+            }
+          });
+      });
+  });
 }
